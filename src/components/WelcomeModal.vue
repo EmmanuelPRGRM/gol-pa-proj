ced099b19bdc2016d34d299b79a08d7d328d9493
Giemel Morales <moralesgiemel@gmail.com>
2021-03-24 14:57:31 +0800

    game on button modification

--- src/components/WelcomeModal.vue
+++ src/components/WelcomeModal.vue
@@ -9,9 +9,8 @@
     <!-- <v-img  v-on:click="promptLogic" contain src="../assets/lobby/welcome-prompt.png" class="modal"> </v-img> -->
     <div class="bg-container">
         <div class="text-wrapper text-center">
-            <p>HI, {{ this.$store.firstName }}!</p>
-            <p>WELCOME TO</p>
-            <p>ONE CP: DIGITAL EVENT</p>
+            <p class="text-name">HI, {{ this.$store.firstName }}!</p>
+            <p class="text-welcome">WELCOME TO<br>ONE CP: DIGITAL EVENT</p>
             <p class="join">JOIN <span class="yellow-text">{{ team }}</span> IN THEIR NEW MISSION.</p>
         </div>
        <!-- <div class="game-on-btn"></div> -->
@@ -60,12 +59,23 @@ export default {
 .text-wrapper{
     color: #00FFFF;
     font-family: "Azonix";
-    font-size: 150%;
 }
+
+.text-name{
+  font-size: 150%;
+}
+
+.text-welcome{
+  font-size: 200%;
+  line-height: 1.2;
+  letter-spacing: 1px;
+}
+
 .join{
     color:white;
     font-family: "Azonix";
-    font-size: 50%;
+    font-size: 70%;
+    letter-spacing: 1px;
 }
 .yellow-text{
     color: #FF990D;
@@ -73,18 +83,22 @@ export default {
 .game-on-btn{
     padding: 0;
     margin: 0;
-    color: #FF990D;
+    color: #f5e500;
     background-color: rgb(37, 37, 37) !important;
-    border: 5px solid #FF990D;
+    border: 2px solid#f5b400 ;
     border-radius: 0px;
     font-family: "oceanicdriftlaser";
-    font-size: 28px;
+    font-size: 35px;
     width: 30%;
-    /* height: 300px; */
+    height: 15%;
     /* padding: 30px; */
-    box-shadow: 0 0 10px rgb(255, 0, 0);
+    box-shadow: 0 0 20px rgb(255, 0, 0);
     text-shadow: 0 0 10px rgb(255, 0, 0);
     /* max-width: 200px; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 .game-on-btn:hover{
     cursor: pointer;
