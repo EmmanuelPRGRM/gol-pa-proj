2fc9ecc00fbfd7fe0a10f1e40c96fe9bd0924588
Giemel Morales <moralesgiemel@gmail.com>
2021-03-25 19:43:01 +0800

    includes GameFrame and LeaderBoardFrame getToken

--- src/components/LeaderboardFrame.vue
+++ src/components/LeaderboardFrame.vue
@@ -1,37 +1,39 @@
 <template>
-    <v-app>
-        <v-main>
-                <iframe
-                :style="{
-                position:'absolute',
-                width: '100%',
-                height: '100%',
-                justify: 'center',
-                border: 'none',
-                    }"
-                id="unitygame-frame"
-                src="https://leaderboard.onecpthedigitalevent2021.com">
-                </iframe>
-        </v-main>
-         <orientation-blocker></orientation-blocker>
-    </v-app>
+  <v-app>
+    <v-main>
+      <iframe
+        :style="{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          justify: 'center',
+          border: 'none',
+        }"
+        id="unitygame-frame"
+        :src="`https://leaderboard.onecpthedigitalevent2021.com?token=${getToken()}`"
+      >
+      </iframe>
+    </v-main>
+    <orientation-blocker></orientation-blocker>
+  </v-app>
 </template>
 
 <script>
 export default {
-
-     components: {
+  components: {
     OrientationBlocker: () => import("@/components/OrientationBlocker.vue"),
   },
-    
-}
+  methods: {
+    getToken: function () {
+      console.log(localStorage.getItem("auth_token"));
+      return localStorage.getItem("auth_token");
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-iframe
-{
-    overflow-y: hidden;
+iframe {
+  overflow-y: hidden;
 }
-
 </style>
